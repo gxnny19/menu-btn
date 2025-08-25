2492949ff1e5fe85adff2d79b6cc78aec24fe2e1
gxnny19 <wkdrudrjs010@gmail.com>
2025-08-25 15:05:08 +0900

    vue

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<script setup>
+import { ref } from 'vue'
+import Ex1 from './components/Ex1.vue';
+import Ex2 from './components/Ex2.vue';
+import Ex3 from './components/Ex3.vue';
+import Ex4 from './components/Ex4.vue';
+
+const pages = { Ex1, Ex2, Ex3, Ex4 }
+const activeComp = ref("Ex1")
+</script>
+
+<template>
+  <h1>장경건의 VUE 과제방 ↓</h1>
+  <!-- <Ex1 />
+  <Ex2 />
+  <Ex3 />
+  <Ex4 /> -->
+  <button @click="activeComp = 'Ex1'">bind</button>
+  <button @click="activeComp = 'Ex2'">if</button>
+  <button @click="activeComp = 'Ex3'">show</button>
+  <button @click="activeComp = 'Ex4'">for</button>
+
+  <div id="comp_view">
+      <KeepAlive>
+        <component :is="pages[activeComp]"></component>
+      </KeepAlive>
+  </div>
+</template>
+
+<style scoped>
+button{
+  margin: 5px;
+  filter: drop-shadow(0 0 1.5px rgb(58, 56, 56));
+}
+#comp_view {
+  margin-top: 20px;
+  border: 1px solid orange;
+  border-radius: 10px;
+}
+</style>
---
+++ src/components/Ex2.vue
@@ -0,0 +1,42 @@
+<template>
+    <h1>if</h1>
+   <hr>
+   <p v-if="typewritersInStock">재고 있음</p>
+   <p v-else>재고 없음</p>
+   <hr>
+   <p v-if="typewriterCount > 0">재고 있음</p>
+   <p v-else>재고 없음</p>
+   <hr>
+   <p v-if="typewriterCount2 > 3">재고 있음</p>
+   <p v-else-if="typewriterCount2 > 0">재고 조금 남음</p>
+   <p v-else-if="typewriterCount2 == 0">재고 없음</p>
+   <p v-else>선불(예약) 있음 -> ({{ typewriterCount2 }})</p>
+   <hr>
+   <p v-if="text.includes('pizza')">"pizza" 글자가 포함되어 있어요</p>
+   <p v-else>"pizza" 글자 포함</p>
+   <hr>
+   <div v-if="text2.includes('피자')">
+    <p>피자</p>
+    <img src="../assets/img_pizza.svg" alt="피자이미지">
+   </div>
+   <p v-else>피자 없음</p>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const typewritersInStock = ref(true) 
+const typewriterCount = ref(0)
+const typewriterCount2 = ref(-2)
+
+const text = ref("I like Taco, pizza, Thai beef salad, poh soup and tagine.")
+const text2 = ref("I like Taco, 피자, Thai beef salad, poh soup and tagine.")
+
+</script>
+
+<style scoped>
+img {
+    width: 150px;
+    height: 150px;
+}
+</style>
